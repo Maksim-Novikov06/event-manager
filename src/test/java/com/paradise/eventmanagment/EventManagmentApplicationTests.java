6e456ccf41fdbd666f4f133ecbd6e7031e0f85c8
maksi <maksimnovikov675@gmail.com>
2025-05-15 13:18:53 +0300

    First commit

---
+++ src/test/java/com/paradise/eventmanagment/EventManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradise.eventmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
